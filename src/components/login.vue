5d68b99e93631023c5c8dfe66abacd0eb9009096
w-1311 <1620575193@qq.com>
2019-03-30 12:00:53 +0800

    整合ui框架css预处理

--- src/components/login.vue
+++ src/components/login.vue
@@ -8,6 +8,12 @@ export default {
 }
 </script>
 
-<style >
- 
+<style lang='scss'>
+// ??css???????lang='scss'  
+// ?? cnpm install -D sass-loader node-sass
+   .login{
+    width:100px; 
+    height: 100px;
+    background-color: skyblue;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入饿了么ui cnpm i element-ui --save
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 // 导入路由
