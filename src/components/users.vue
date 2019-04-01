4028cf0c8857fb09470aa57c9ae11cd679126ec9
w-1311 <1620575193@qq.com>
2019-04-01 20:20:53 +0800

    编辑用户&删除用户

--- src/components/users.vue
+++ src/components/users.vue
@@ -50,7 +50,13 @@
             @click="handleEdit(scope.$index,scope.row)"
             plain
           ></el-button>
-          <el-button type="danger" size="mini" icon="el-icon-delete" plain></el-button>
+          <el-button
+            @click="delOne(scope.row)"
+            type="danger"
+            size="mini"
+            icon="el-icon-delete"
+            plain
+          ></el-button>
           <el-button type="warning" size="mini" icon="el-icon-check" plain></el-button>
         </template>
       </el-table-column>
@@ -84,6 +90,25 @@
         <el-button type="primary" @click="submitAdd('addForm')">确 定</el-button>
       </div>
     </el-dialog>
+
+    <!-- 编辑弹框 -->
+    <el-dialog title="编辑用户" :visible.sync="editFormVisible">
+      <el-form :model="editForm" :rules="addRules" ref="editForm">
+        <el-form-item label="用户名" label-width="100px" prop="username">
+          <el-input v-model="editForm.username" disabled autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="editForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="editForm.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitEdit('editForm')">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -104,10 +129,18 @@ export default {
       userList: [],
       // 是否显示新增对话框
       addFormVisible: false,
+      // 是否显示编辑对话框
+      editFormVisible: false,
       // 新增表单
       addForm: {
         username: "新增",
         password: "123456",
+        email: "littleice@qq.com",
+        mobile: "18888888888"
+      },
+      // 编辑表单
+      editForm: {
+        username: "编辑",
         email: "littleice@qq.com",
         mobile: "18888888888"
       },
@@ -128,6 +161,13 @@ export default {
     async handleEdit(index, row) {
       console.log(index); // 索引
       console.log(row); // 数据
+      let res = await this.$axios.get(`users/${row.id}`);
+      // console.log(res);
+      this.editForm = res.data.data;
+      // 弹框
+      if (res.data.meta.status === 200) {
+        this.editFormVisible = true;
+      }
     },
     // 搜索用户
     async search() {
@@ -150,18 +190,63 @@ export default {
     },
     // 提交新增
     submitAdd(formName) {
-      this.$refs[formName].validate(valid => {
+      this.$refs[formName].validate(async valid => {
         if (valid) {
-          alert("submit!");
+          // alert("submit!");
+          // 接口调用
+          let res = await this.$axios.post("users", this.addForm);
+          // console.log(res);
+          // 如果新增成功 重新获取用户列表
+          if (res.data.meta.status === 201) {
+            this.search();
+          }
+          // 关闭弹框
+          this.addFormVisible = false;
         } else {
           this.$message.error("哥们，亲正确输入数据");
           return false;
         }
       });
+    },
+    // 保存修改
+    async submitEdit(formName) {
+      // 修改的字段可以为空 这个不用表单验证
+      // 直接发请求即可
+      let res = await this.$axios.put(`users/${this.editForm.id}`, {
+        email: this.editForm.email,
+        mobile: this.editForm.mobile
+      });
+      // console.log(res);
+      if (res.data.meta.status === 200) {
+        this.search();
+      }
+      // 关闭弹框
+      this.editFormVisible = false;
+    },
+    // 删除用户
+    delOne(row) {
+      this.$confirm("亲，你真的要永久删除该用户, 吗 o(╥﹏╥)o?", "提示", {
+        confirmButtonText: "狠心删除",
+        cancelButtonText: "良心发现",
+        type: "warning"
+      })
+        .then(async () => {
+          // axios
+          let res = await this.$axios.delete(`users/${row.id}`);
+          if (res.data.meta.status === 200) {
+            this.search();
+          }
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "你真好o(^▽^)o"
+          });
+        });
     }
   },
   // 接口调用
-  async created() {
+  created() {
     this.search();
   }
 };
