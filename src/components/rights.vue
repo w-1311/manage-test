f9d8ef6251ed6c0e45b41c68b90ce8df23b2ff52
w-1311 <1620575193@qq.com>
2019-04-03 16:20:49 +0800

    权限列表数据渲染

--- src/components/rights.vue
+++ src/components/rights.vue
@@ -4,26 +4,34 @@
     <my-bread sectitle="权限管理" threetitle="权限列表"></my-bread>
 
     <!-- 表格 -->
-    <el-table :data="rightList"  style="width: 100%" border>
+    <el-table :data="rightList" style="width: 100%" border>
       <el-table-column type="index" width="50"></el-table-column>
-      <el-table-column  label="权限名称" width="180"></el-table-column>
-      <el-table-column label="路径" width="180"></el-table-column>
-      <el-table-column label="层级" width="180"></el-table-column>
+      <el-table-column prop="authName" label="权限名称" width="180"></el-table-column>
+      <el-table-column prop="path" label="路径" width="180"></el-table-column>
+      <el-table-column prop="level" label="层级" width="180">
+        <template slot-scope="scope">
+          <span v-if="scope.row.level==='0'">一级</span>
+          <span v-else-if="scope.row.level==='1'">二级</span>
+          <span v-else>三级</span>
+        </template>
+      </el-table-column>
     </el-table>
-
-
   </div>
 </template>
 
 <script>
 export default {
   name: "rights",
-   data(){
+  data() {
     return {
-      rightList:[{},{}]
-    }
+      rightList: [{}, {}]
+    };
+  },
+  async created() {
+    let res = await this.$axios.get("rights/list");
+    console.log(res);
+    this.rightList = res.data.data;
   }
-
 };
 </script>
 
