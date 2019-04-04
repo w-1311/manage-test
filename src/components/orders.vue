52c1739ec731425f4d7f1b609b61b444c7e9dcd1
w-1311 <1620575193@qq.com>
2019-04-04 09:34:02 +0800

    订单列表省市联动数据

--- src/components/orders.vue
+++ src/components/orders.vue
@@ -23,7 +23,13 @@
         <!-- scope 是一个名字 -->
         <template slot-scope="scope">
           <!-- 我们可以通过scope.$index 获取索引 scope.row获取这一行的数据 -->
-          <el-button type="primary" size="mini" icon="el-icon-edit" plain></el-button>
+          <el-button
+            @click="dialogFormVisible=true"
+            type="primary"
+            size="mini"
+            icon="el-icon-edit"
+            plain
+          ></el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -37,10 +43,33 @@
       @size-change="sizeChange"
       @current-change="currentChange"
     ></el-pagination>
+    <!-- 对话框 -->
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="省市区" :label-width="formLabelWidth">
+          <!-- 级联选择器实现 -->
+          <el-cascader :options="options" v-model="selectedOptions"></el-cascader>
+          <!-- v-distpicker实现 -->
+          <v-distpicker></v-distpicker>
+        </el-form-item>
+        <el-form-item label="详细地址" :label-width="formLabelWidth">
+          <el-input placeholder></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+// 导入省市联动数据
+import options from "../assets/city_data2017_element.js";
+// 导入组件
+import VDistpicker from "v-distpicker";
+
 export default {
   name: "users",
   data() {
@@ -49,10 +78,21 @@ export default {
         pagesize: 10,
         pagenum: 1
       },
+      // 弹框标记字段
+      dialogFormVisible: false,
+      // 渲染的数据
+      options,
+      // 级联选择器绑定的数据
+      selectedOptions: [],
       // 总条数
       total: 0,
       orderList: [{}, {}]
     };
+  },
+
+  // 本地注册组件
+  components: {
+    VDistpicker
   },
 
   // 方法
